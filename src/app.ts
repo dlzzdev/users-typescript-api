6f6d494329bebad5913097dc3e33c0f58fe0209e
Alisson Felipe <alissonfelipenettt@gmail.com>
2022-12-17 14:38:27 -0300

    feat: add express initial server & create model prisma

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("");
+import express from "express";
+import { config } from "dotenv";
+config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
